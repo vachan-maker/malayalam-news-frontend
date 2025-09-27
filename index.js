2bd4dc0928bb8b09a411f523889255b6df381da0
vachan-maker <vachan@tutanota.com>
2025-09-27 21:29:25 +0530

    error handling

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {engine} from 'express-handlebars'
+import { engine } from 'express-handlebars'
 import axios from 'axios'
 import * as cheerio from 'cheerio'
 
@@ -13,47 +13,57 @@ app.set('views', './views');
 
 async function fethData() {
     const headlines = []
-    const response = await axios(url)
-    const data = await response.data
-    const $ = cheerio.load(data)
-    console.log(data)
-    $('.cmp-story-list__title-link').each((index,element)=>{
-        const headline = $(element).attr('title')
-        const link = $(element).attr('href') 
-        headlines.push({headline,link})
-    })
+    try {
+        const response = await axios(url)
+        const data = await response.data
+        const $ = cheerio.load(data)
+        console.log(data)
+        $('.cmp-story-list__title-link').each((index, element) => {
+            const headline = $(element).attr('title')
+            const link = $(element).attr('href')
+            headlines.push({ headline, link })
+        })
+    } catch (error) {
+        console.log("Error", error)
+        return []
+    }
     return headlines
 
 }
 async function fetchArtcle(url) {
-    const response = await axios(url)
-    const data = await response.data
-    const $ = cheerio.load(data)
     const article = []
+    try {
+        const response = await axios(url)
+        const data = await response.data
+        const $ = cheerio.load(data)
 
-    $('.rtearticle.text p').each((index,element)=>{
-        article.push($(element).text())
-        
-    })
-    const headline = $(`.article-header__title`).text()
-    return ({title:headline,content:article})
+        $('.rtearticle.text p').each((index, element) => {
+            article.push($(element).text())
+
+        })
+        const headline = $(`.article-header__title`).text()
+        return ({ title: headline, content: article })
+    } catch (error) {
+        console.log(error)
+        return ({ title: "Error", content: "Error fetching content" })
+    }
 }
 
-app.get('/',async (req,res)=>{
+app.get('/', async (req, res) => {
     const items = await fethData()
-    res.render('home',{items:items})
+    res.render('home', { items: items })
 })
 
-app.get('/news/latest-news/:year/:month/:day/:headline',async(req,res)=>{
+app.get('/news/latest-news/:year/:month/:day/:headline', async (req, res) => {
     const year = req.params.year
     const month = req.params.month
     const day = req.params.day
     const headline = req.params.headline
     const url = `https://www.manoramaonline.com/news/latest-news/${year}/${month}/${day}/${headline}`
     const article = await fetchArtcle(url)
-    res.render('article',{headline:article.title,content:article.content,url:url})
+    res.render('article', { headline: article.title, content: article.content, url: url })
 
 })
 
 
-app.listen(PORT, ()=>console.log(`Listening on PORT: ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`))
