10c16abf7683499f03506972ca680742b4acdd6f
vachan-maker <vachan@tutanota.com>
2025-09-28 15:20:17 +0530

    Forgot to add viewport and also added description for articles

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.get('/news/latest-news/:year/:month/:day/:headline', async (req, res) => {
     const url = `https://www.manoramaonline.com/news/latest-news/${year}/${month}/${day}/${headline}`
     try{
     const article = await fetchArtcle(url)
-    res.render('article', { headline: article.title, content: article.content, url: url,title:article.title })
+    res.render('article', { headline: article.title, content: article.content, url: url,title:article.title,description:article.content[0] })
     }
     catch(error) {
         res.status(500).send("Internal Error!")
