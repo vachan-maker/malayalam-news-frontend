6d84ac24847d1b8801621280a0b7476a80bd89bf
vachan-maker <vachan@tutanota.com>
2025-09-30 08:44:20 +0530

    trying to fix error by using abs path on vercel

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 import express from 'express'
 import { engine } from 'express-handlebars'
 import router from './routes/routes.js';
+import { fileURLToPath } from 'url';
+import path from 'path';
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
 app.use(express.static('public'))
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './src/views');
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', path.join(__dirname, 'views'))
 
 app.use('/',router)
 
